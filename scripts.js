a9c875963c5aad6570e1120796f3c1827435bc33
amandaeriksson98 <eriksson.amanda5@gmail.com>
2024-09-04 18:17:45 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// scripts.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.querySelector('.hero-content button');
+    button.addEventListener('click', () => {
+        alert('Välkommen att utforska våra sekelskifteshus!');
+    });
+});
